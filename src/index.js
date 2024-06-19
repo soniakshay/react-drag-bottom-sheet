1c5603463cc4f6a71360e479ea66c230609f4320
akshay-rapidops <soniiakshayy8000@gmail.com>
2024-06-19 19:51:54 +0530

    #changes done remove console

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const ReactDragBottomSheet = ({
                                   onAfterClose = null,
                                   shouldCloseOnOverlayClick= true,
                                   shouldCloseOnEsc= true,
+                                  maxHeight='50%',
                                   overlayClassName = '',
                                   className=''
                               }, fRef) =>  {
@@ -16,27 +17,40 @@ const ReactDragBottomSheet = ({
     const [isDragging, setIsDragging] = useState(false);
     const sheetRef = useRef(null);
     const startYRef = useRef(0);
-
+    const [bottomSheetHeight,setBottomSheetHeight] = useState(null)
 
     const handleDragStart = (event) => {
         setIsDragging(true);
         startYRef.current = event.touches ? event.touches[0].clientY : event.clientY;
     };
 
+    useEffect(() => {
+        if(isBottomSheetOpen) {
+            setTimeout(() => {
+                const  bottomSheet = document.getElementsByClassName('bottom-sheet')[0];
+                setBottomSheetHeight(bottomSheet?.clientHeight)
+            },1000)
+
+        }
+
+    },[isBottomSheetOpen])
     const handleDragMove = (event) => {
         if (!isDragging) return;
         const currentY = event.touches ? event.touches[0].clientY : event.clientY;
         const diffY = currentY - startYRef.current;
-
         if (diffY > 0 && sheetRef.current) {
             // @ts-ignore
             sheetRef.current.style.transform = `translateY(${diffY}px)`;
+        } else {
+
+              sheetRef.current.style.maxHeight = `${bottomSheetHeight + Math.abs(diffY)}px`
         }
     };
 
     const handleDragEnd = (event) => {
         if (!isDragging) return;
         setIsDragging(false);
+        sheetRef.current.style.maxHeight = maxHeight;
         const endY = event.touches ? event.touches[0].clientY : event.clientY;
         const diffY = endY - startYRef.current;
 
@@ -146,7 +160,7 @@ const ReactDragBottomSheet = ({
             window.removeEventListener('keydown', handleKeyDown);
 
         };
-    }, [isDragging]);
+    }, [isDragging,bottomSheetHeight]);
 
     useEffect(() => {
         setTimeout(() => {
@@ -174,6 +188,9 @@ const ReactDragBottomSheet = ({
                             className={`bottom-sheet ${animationClass} ${className} ${isDragging ? 'dragging' : ''}`}
                             onClick={(e) => e.stopPropagation()}
                             ref={sheetRef}
+                            style={{
+                                maxHeight:maxHeight
+                            }}
 
                         >
                             <div className={'drag-handle-main'}>
